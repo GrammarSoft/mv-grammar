b37b38c1118a92e8d44bab86ae3471507611b280
Tino Didriksen <mail@tinodidriksen.com>
2017-07-06 11:23:24 +0000

    Grammatikforslag 2017-07-06 11:22:41

--- debug/index.php
+++ debug/index.php
@@ -2,7 +2,7 @@
 
 header('Content-Type: text/plain; charset=UTF-8');
 
-echo "Grammatikforslag at commit 907cf710a970c2a674a7d1250f6fae63052d550c (r139) deployed on 2017-07-05 14:16:48\n\n";
+echo "Grammatikforslag at commit a3fa22b8edd19dffb01fb6ec631207609de52309 (r140) deployed on 2017-07-06 11:22:41\n\n";
 
 echo "uptime:\n", shell_exec('uptime'), "\n";
 echo "free -h:\n", shell_exec('free -h'), "\n";
