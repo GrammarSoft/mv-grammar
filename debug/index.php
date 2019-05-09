dc703d3a447f3af593f7361dfe4885fae12e83de
Tino Didriksen <mail@tinodidriksen.com>
2019-05-09 10:37:15 +0000

    Vitec logo

--- debug/index.php
+++ debug/index.php
@@ -2,7 +2,7 @@
 
 header('Content-Type: text/plain; charset=UTF-8');
 
-echo "Grammateket at r438 committed on 2019-03-20 10:03:45\n\n";
+echo "Grammateket at r440 committed on 2019-05-09 10:31:20\n\n";
 
 echo "uptime:\n", shell_exec('uptime'), "\n";
 echo "free -h:\n", shell_exec('free -h'), "\n";
